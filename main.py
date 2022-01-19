4affa656f998cecb1f4d2b78a2feda6d2c01cb82
maxrel95 <58341766+maxrel95@users.noreply.github.com>
2022-01-19 17:51:56 +0100

    add steps

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+this is the main script for GAN Simulation project.
+First the code is implemented as a script and then would be rewritten in a best format
+"""
+
+## download the data
+
+
+## treat the data
+
+## Write the model GAN
+
+## train the model
+
+## use the model
